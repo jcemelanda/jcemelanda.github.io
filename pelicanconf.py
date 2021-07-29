d0c35a5ee09bd01586938b353c1c684aa63c2451
Julio Cesar Eiras Melanda <jcemelanda@gmail.com>
2021-07-30 01:51:46 +0200

    Adds first post and fixes css

---
+++ pelicanconf.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+import os
+from datetime import datetime
+
+AUTHOR = "Julio Melanda"
+SITENAME = "Julio Cesar Eiras Melanda"
+SITETITLE = "Julio Melanda's Blog"
+SITEURL = ""
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+PATH = "content"
+
+TIMEZONE = "Europe/Copenhagen"
+
+DEFAULT_LANG = "en"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (("Want to contract me?", "https://jcemelanda.github.io/curriculum/"),)
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
+
+THEME = "Flex"
+
+TYPOGRIFY = True
+
+ROBOTS = "index, follow"
+
+COPYRIGHT_YEAR = datetime.today().year
+
+USE_FOLDER_AS_CATEGORY = False
+MAIN_MENU = True
+HOME_HIDE_TAGS = True
+
+MENUITEMS = (
+    ("Archives", "/archives.html"),
+    ("Categories", "/categories.html"),
+    ("Tags", "/tags.html"),
+)
+
+# Enable i18n plugin.
+PLUGINS = ["i18n_subsites"]
+# Enable Jinja2 i18n extension used to parse translations.
+JINJA_ENVIRONMENT = {"extensions": ["jinja2.ext.i18n"]}
+
+# Default theme language.
+I18N_TEMPLATES_LANG = "en"
+
+PLUGIN_PATHS = [os.path.join("..", "pelican-plugins")]
+
+SITELOGO = SITEURL + "/images/Julio.jpg"
+
+THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
+THEME_COLOR_ENABLE_USER_OVERRIDE = True
+
+DATE_FORMATS = {
+    "en": "%B %d, %Y",
+}
+BROWSER_COLOR = "#666666"
+PYGMENTS_STYLE = "github"
+
+SOCIAL = (
+    ("github", "https://github.com/jcemelanda"),
+    ("linkedin", "https://www.linkedin.com/in/jcemelanda"),
+    ("twitter", "https://twitter.com/jcemelanda"),
+)
+
+STATIC_PATHS = ["images", "extra/custom.css"]
+
+EXTRA_PATH_METADATA = {
+    "extra/custom.css": {"path": "static/custom.css"},
+}
+
+CUSTOM_CSS = "static/custom.css"
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
